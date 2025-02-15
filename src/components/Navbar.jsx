2a64e6db67cdf3252501d3d4b07ae3ea929eb2dc
sahajsatani <sahajsatani2005@gmail.com>
2025-02-15 15:22:25 +0530

    cart

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -70,7 +70,7 @@ const Navbar = ({ setData, cart }) => {
             />
           </form>
 
-          <Link to={"/cart"} className="cart">
+          <Link to={"/cart"} id="cartbtnlink" className="cart">
             <button type="button" id="cartbtn" className="btn btn-primary position-relative">
               <BsFillCartCheckFill style={{ fontSize: "1.5rem" }} />
               <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
